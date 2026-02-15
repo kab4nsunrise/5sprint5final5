bd628a0ec6e6eeb8adba7d0b5befd3e522b96d5b
Артем <kotbegemot001@yandex.ru>
2026-02-15 22:25:12 +0500

    Update daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,71 @@
 package daysteps
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
+
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 2 {
+		
+		return fmt.Errorf("invalid data format")
+	}
+
+	
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return err
+	}
+
+	
+	if steps <= 0 {
+		return fmt.Errorf("шаги должны быть положительным числом")
+	}
+
+	duration, err := time.ParseDuration(parts[1])
+	if err != nil {
+		return err
+	}
+	if duration <= 0 {
+		return fmt.Errorf("длительность должна быть положительной")
+	}
+
+
+	return nil
 }
 
-func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+
+	calories, err := spentenergy.WalkingSpentCalories(
+		ds.Steps,
+		ds.Weight,
+		ds.Height,
+		ds.Duration,
+	)
+	if err != nil {
+	
+		return "", err
+	}
+
+	return fmt.Sprintf(
+		"Количество шагов: %d.\n"+
+			"Дистанция составила %.2f км.\n"+
+			"Вы сожгли %.2f ккал.\n",
+		ds.Steps,
+		distance,
+		calories,
+	), nil
 }
